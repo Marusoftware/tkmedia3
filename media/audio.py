7ee9bf1f82f01d372eb1b27991320a64ca437b54
Marusoftware <marusoftware@outlook.jp>
2021-02-16 14:02:04 +0900

    bug fix?

--- media/audio.py
+++ media/audio.py
@@ -35,15 +35,16 @@ class Audio():#TODO: Stop, Change
                 try:
                     tmp_frame=next(l)
                 except StopIteration:
-                    pass
+                    if not self.soundStream.active:
+                        self.soundStream.start()
                 else:
                     self.playback["Now"]["index"]=tmp_frame.index
                     self.finished=False
-                    if tmp_frame.index*tmp_frame.samples < tmp_frame.samples*5:
-                        border = tmp_frame.index*tmp_frame.samples
+                    if aud_info["frame_count"]-tmp_frame.index <= 5:
+                        border = (aud_info["frame_count"]-tmp_frame.index)*tmp_frame.samples
                     else:
                         border = tmp_frame.samples*5
-                    if self.fifo.samples <= border:#TODO: sync!
+                    if self.fifo.samples < border:#TODO: sync!
                         if self.soundStream.active:
                             self.soundStream.abort()
                         if self.playback["syncV"] != None and vstate == "play":
@@ -97,83 +98,3 @@ class Audio():#TODO: Stop, Change
             self.state = value
         elif key == "clr_cache":
             self.playback.pop("temp_stream")
-
-""" class Video():
-    def _Show(self):
-        if state == "pause":
-            #self.watch.stop()
-            self.playback["v_Frame"].after(int(sleep_time*1000), self._Show)
-            return
-        elif state == "play":
-            self.watch.start()
-            if "temp_stream" in self.playback:
-                l = self.playback["temp_stream"]
-            else:
-                #f = Filter.Graph()
-                #f.add_buffer(template=self._ffmpeg.streams.video[streamN])
-                #f.add("pad",":".join([str(self.playback["v_dispWidth"]),str(self.playback["v_dispHeight"])]))
-                #f.add("buffersink")
-                #f.configure()
-                l = self._ffmpeg.av.decode(video=streamN)
-                self.playback["temp_stream"]=l
-            
-            if "temp_image" in self.playback:
-                try:
-                    self.playback["temp_image"].close()
-                    self.playback["temp_image2"].close()
-                    #self.playback.pop("temp_image")
-                except:
-                    pass
-            try:
-                #tmp_frame=next(l).reformat(width=self.playback["v_dispWidth"],height=self.playback["v_dispHeight"])
-                tmp_frame=next(l)
-            except StopIteration:
-                self.playback["state"]="stop"
-                self.playback["v_Frame"].after(0, self._Show)
-            else:
-                if sleep_time*tmp_frame.index >= self.watch.getTime():
-                    self.playback["temp_image"]=tmp_frame.to_image()
-                    self.playback["temp_image"].thumbnail((self.playback["v_dispWidth"], self.playback["v_dispHeight"]))
-                    self.playback["temp_image2"] = Image.new("RGBA",[self.playback["v_dispWidth"],self.playback["v_dispHeight"]],(0,0,0,255))
-                    self.playback["temp_image2"].paste(self.playback["temp_image"],(int((self.playback["v_dispWidth"]-self.playback["temp_image"].size[0])/2),int((self.playback["v_dispHeight"]-self.playback["temp_image"].size[1])/2)))
-                    self.playback["temp_imageTk"] = ImageTk.PhotoImage(self.playback["temp_image2"], master=self.playback["v_Frame"])
-                    if self.playback["v_FrameType"] == "frame":
-                        self.playback["v_Frame"].configure(image=self.playback["temp_imageTk"])
-                    self.playback["v_Frame"].after(int(sleep_time*1000), self._Show)
-                else:
-                    self.playback["v_Frame"].after(0, self._Show)
-            #self.playback["v_time"]+=(time.time()-t1)
-            return
-        elif state == "reload":
-            self.watch.stop()
-            l = self._ffmpeg.av.decode(video=streamN)
-            self.playback.update(state="play")
-        elif state == "stop":
-            self.watch.stop()
-            self.watch.clear()
-            return
-        else:
-            self.playback.update(state="pause")
-        self.playback["v_Frame"].after(0, self._Show)
-    def Show(self, frame, streamN=0, start_point=0, speed=1, frameType="frame", thread_type="AUTO", height=600, width=600):
-        self.playback["v_streamN"] = streamN
-        self.playback["v_Frame"] = frame
-        self.playback["v_FrameType"] = frameType
-        self.playback["v_Point"] = start_point
-        self.playback["v_dispHeight"] = height
-        self.playback["v_dispWidth"] = width
-        self.playback["v_time"] = 1
-        self.playback["state"] = "play"
-        self.playback["v_speed"] = speed
-        self._ffmpeg.streams.video[streamN].thread_type=thread_type
-        frame.after(0, self._Show)
-    def Stop(self):
-        self.playback["state"] = "stop"
-    def Change(self, key, value=None):
-        if key == "stream":
-            self.v_streamN = int(value)
-            self.Change("clr_cache")
-        elif key == "state":
-            self.state = value
-        elif key == "clr_cache":
-            self.playback.pop("temp_stream") """
