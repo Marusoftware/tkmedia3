bf6187d52d12874d2a5e0e0d4d521f28b552fa7d
Marusoftware <marusoftware@outlook.jp>
2021-02-23 01:40:43 +0900

    fix filedialog

--- media/lib.py
+++ media/lib.py
@@ -1,13 +1,21 @@
 import time
 import platform
 import tkinter.filedialog as _fd1
-if platform.system() == "Linux":
-    import tkfilebrowser as _fd2
+fdmode=0
+if platform.system() in ["Linux", "Darwin"]:
+    try:
+        import tkfilebrowser as _fd2
+    except:
+        fdmode=0
+    else:
+        fdmode=1
 else:
     try:
         import tkfilebrowser as _fd2
     except:
-        pass
+        fdmode=0
+    else:
+        fdmode=1
 
 class StopWatch():
     def __init__(self, error=True):
@@ -41,31 +49,27 @@ class StopWatch():
         if self.isStop:
             self.time = t
 
-#default setting
-if platform.system() == "Linux":
-    mode = 1
-else:
-    mode = 0
-
 #dialogs
 def askdirectory(fd=None, **argv):
-    if fd == None: fd = mode
+    if fd == None: fd = fdmode
     if fd:
         return _fd2.askopendirname(**argv)
     else:
         return _fd1.askdirectory(**argv)
 
 def askopenfilename(fd=None, **argv):
-    if fd == None: fd = mode
-    if platform.system() == "Darwin":
-        #load_fd2()
-        return _fd2.askopenfilename(**argv)
-    if fd:
+    if fd == None: fd = fdmode
+    if platform.system() == "Darwin" and "filetypes" in argv:
+        if len(argv["filetypes"])==1 and argv["filetypes"][0][1]=="*.*" and fdmode:
+            return _fd2.askopenfilename(**argv)
+        else:
+            return _fd1.askopenfilename(**argv)
+    elif fd:
         return _fd2.askopenfilename(**argv)
     else:
         return _fd1.askopenfilename(**argv)
 def askopenfilenames(fd=None, **argv):
-    if fd == None: fd = mode
+    if fd == None: fd = fdmode
     if platform.system() == "Darwin":
         #load_fd2()
         return _fd2.askopenfilenames(**argv)
@@ -74,7 +78,7 @@ def askopenfilenames(fd=None, **argv):
     else:
         return _fd1.askopenfilenames(**argv)
 def asksaveasfilename(fd=None, **argv):
-    if fd == None: fd = mode
+    if fd == None: fd = fdmode
     if fd:
         return _fd2.asksaveasfilename(**argv)
     else:
